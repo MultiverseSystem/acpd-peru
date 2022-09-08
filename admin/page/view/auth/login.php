1a534042ffdb519b3005339f46496789d46ee97c
MultiverseSystem <juan.nc.ms24@gmail.com>
2022-09-08 13:10:38 -0500

    ajustes login

--- admin/page/view/auth/login.php
+++ admin/page/view/auth/login.php
@@ -1,26 +1,29 @@
+<section style="height: 100vh; background:#0A0053;" class="d-flex justify-content-center align-items-center">
 
-
-<div class="container mb-5">
-    <div class="">
-        <div class="rounded d-flex justify-content-center">
-            <div class="col-md-6 col-sm-12 shadow-lg p-5 bg-light mt-5">
-                <div class="text-center">
-                    <h3 class="text-primary">Iniciar sesión</h3>
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-5 text-center">
+                <img src="page/images/02.png" width="300" alt="iglesia">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="text-center">
+                            <h3 class="text-primary">Iniciar sesión</h3>
+                        </div>
+                        <form class="form" id="inicio-sesion-admin">
+                            <div class="input-group mb-3">
+                                <input type="email" class="form-control" placeholder="Correo Electronico" id="correo-admin" name="correo-admin">
+                            </div>
+                            <div class="input-group mb-3">
+                                <input type="password" class="form-control" placeholder="Contraseña" id="password-admin" name="password-admin">
+                            </div>
+                            <button class="btn btn-primary btn-block" type="submit">
+                                Iniciar sesión
+                            </button>
+                        </form>
+                    </div>
                 </div>
-                <form class="form" id="inicio-sesion-admin">
-                    <div class="input-group mb-3">
-
-                        <input type="email" class="form-control" placeholder="Correo Electronico" id="correo-admin" name="correo-admin">
-                    </div>
-                    <div class="input-group mb-3">
-
-                        <input type="password" class="form-control" placeholder="Contraseña" id="password-admin" name="password-admin">
-                    </div>
-                    <button class="btn btn-primary btn-block" type="submit">
-                        Iniciar sesión
-                    </button>
-                </form>
             </div>
         </div>
     </div>
-</div>
+
+</section>
